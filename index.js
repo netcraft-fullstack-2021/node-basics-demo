bd62f613a748962a1999e135cffdabf270acfa5a
Ran <ran.wahle@gmail.com>
2022-02-25 19:18:38 +0200

    Use padLeft

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { flatten } from "./array-utils.js";
+import padLeft from 'pad-left';
 
 const arrayOfArrays = [[1,2,3], [4, 5,6], [7,8,9]]
 
-console.log(flatten(arrayOfArrays));
+console.log(flatten(arrayOfArrays).map(item => padLeft(item, 4, '*') ));
+
+//output
